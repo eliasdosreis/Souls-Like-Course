19fb4c7d6920d2e5a7ead8b3c80415380b05dfde
Elias Lima <elias_dos_reis@hotmail.com>
2020-03-24 13:16:09 -0300

    Adicionando Assets + Configurando Animacoes
    Configurando mascara de corpo, criando movimentação vertical, e configurando ataques.

---
+++ Assets/Scripts/Utilities/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	namespace SA
+	{
+	public class Helper : MonoBehaviour {
+		[Range(0,1)] public float vertical;
+
+		public string animName;
+		public bool	playAnim;
+
+		public bool twoHanded;
+
+		Animator anim;
+
+
+		void Start () {
+			anim = GetComponent<Animator> ();
+		}
+
+		void Update () {
+			anim.SetBool ("two_handed", twoHanded);
+
+			if (playAnim) {
+				vertical = 0;
+				anim.CrossFade (animName,0.2f);
+				playAnim = false;	
+			}
+
+			anim.SetFloat ("vertical", vertical);
+		}
+   }
+}
